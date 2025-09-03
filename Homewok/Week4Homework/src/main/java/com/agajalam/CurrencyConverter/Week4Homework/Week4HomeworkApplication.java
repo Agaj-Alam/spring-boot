eb795fdea1bd6b0a336220c670c559ac8d7a9d77
Agaj Alam <agajalam283@gmail.com>
2025-09-03 15:06:46 +0530

    week4homework

---
+++ Homewok/Week4Homework/src/main/java/com/agajalam/CurrencyConverter/Week4Homework/Week4HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.agajalam.CurrencyConverter.Week4Homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4HomeworkApplication.class, args);
+	}
+
+}
---
+++ Homewok/Week4Homework/src/test/java/com/agajalam/CurrencyConverter/Week4Homework/Week4HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agajalam.CurrencyConverter.Week4Homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
