81cc78f57309cd48fc7701535f5bcb18e4769e0f
Agaj Alam <agajalam283@gmail.com>
2025-09-20 23:40:28 +0530

    security

--- Week6/src/main/java/com/agajalam/SecurityApp/Week5/configs/WebSecurityConfig.java
+++ Week6/src/main/java/com/agajalam/SecurityApp/Week5/configs/WebSecurityConfig.java
@@ -44,15 +44,7 @@ public class WebSecurityConfig {
         httpSecurity
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers(publicRoutes).permitAll()
-                        .requestMatchers(HttpMethod.GET,"/posts/**").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/posts/**")
-                                        .hasAnyRole(ADMIN.name(),CREATOR.name())
-                        .requestMatchers(HttpMethod.POST,"/posts/**")
-                                .hasAnyAuthority(POST_CREATE.name())
-                        .requestMatchers(HttpMethod.PUT,"/posts/**")
-                                .hasAnyAuthority(POST_UPDATE.name())
-                        .requestMatchers(HttpMethod.DELETE,"/posts/**")
-                                .hasAnyAuthority(POST_DELETE.name())
+                        .requestMatchers("/posts/**").authenticated()
                         .anyRequest().authenticated())
                 .csrf(csrfConfig->csrfConfig.disable())
                 .sessionManagement(sessionConfig->sessionConfig
