4c3c1781fee9e05e6f31b938d71ffca82089fe20
Agaj Alam <agajalam283@gmail.com>
2025-09-24 01:32:05 +0530

    testing app

--- TestingApp/src/test/java/com/agajalam/week7/TestingApp/TestingAppApplicationTests.java
+++ TestingApp/src/test/java/com/agajalam/week7/TestingApp/TestingAppApplicationTests.java
@@ -1,17 +1,50 @@
 package com.agajalam.week7.TestingApp;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class TestingAppApplicationTests {
 
+    @AfterEach
+    void tearDown(){
+        log.info("Tearing down the method");
+    }
+
+    @BeforeAll
+    static void setUpOnce(){
+        log.info("setup once....");
+    }
+
+    @AfterAll
+    static void tearDownOnce(){
+        log.info("Tearing down all");
+    }
+
 	@Test
-	void contextLoads() {
+//    @Disabled
+	void testNumberOne() {
+        int a=2;
+        int b=4;
+        int result=addTwoNumbers(a,b);
+
+//        Assertions.assertEquals(6,result);
+
+        Assertions.assertThat(result).isEqualTo(7).isCloseTo(9, Offset.offset(3));
 	}
 
+    @Test
+//    @DisplayName("DisplayTestNameTwo")
     void testNumber2(){
+        log.info("test number two ");
+    }
 
+    int addTwoNumbers(int a, int b){
+        return a+b;
     }
 
 }
