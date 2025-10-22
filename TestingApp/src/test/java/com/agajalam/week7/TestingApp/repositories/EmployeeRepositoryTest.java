e72ee892d1542ddb362276ac4d3accaf3fbaf636
Agaj Alam <agajalam283@gmail.com>
2025-10-22 22:22:14 +0530

    testing app

--- TestingApp/src/test/java/com/agajalam/week7/TestingApp/repositories/EmployeeRepositoryTest.java
+++ TestingApp/src/test/java/com/agajalam/week7/TestingApp/repositories/EmployeeRepositoryTest.java
@@ -1,12 +1,51 @@
 package com.agajalam.week7.TestingApp.repositories;
 
+import com.agajalam.week7.TestingApp.entities.Employee;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+
+@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class EmployeeRepositoryTest {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    private Employee employee;
+
+    @BeforeEach
+    void setUp(){
+        employee=Employee.builder()
+                .id(1L)
+                .name("Agaj")
+                .email("agajalam283@gmail.com")
+                .salary(100L)
+                .build();
+    }
+
     @Test
-    void findByEmail() {
+    void testFindByEmail_whenEmailIsPresent_thenReturnEmployee() {
+//        Arrange.  Given
+            employeeRepository.save(employee);
+
+//        Act. when
+            List<Employee> employeeList=employeeRepository.findByEmail(employee.getEmail());
+
+//        Assert.  Then
+
+
+    }
+
+
+    @Test
+    void testFindByEmail_whenEmailIsNotFound_thenReturnEmptyEmployeeList(){
+
     }
 }
